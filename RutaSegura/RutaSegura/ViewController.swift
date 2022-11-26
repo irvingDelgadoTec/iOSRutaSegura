4d3aeb61a5b024428d3ddd71dd9d3f91d9f1d6d3
Irving Delgado <irving.delgado@wizeline.com>
2022-11-25 23:27:14 -0600

    Added empty project

---
+++ RutaSegura/RutaSegura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RutaSegura
+//
+//  Created by Irving Delgado Silva on 25/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+
+
+}
+
