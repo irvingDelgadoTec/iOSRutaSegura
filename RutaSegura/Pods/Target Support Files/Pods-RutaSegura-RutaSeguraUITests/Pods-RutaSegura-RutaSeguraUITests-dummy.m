3c9a89a46e8dda3a5f707b7b13c974f472137d1b
Irving Delgado <irving.delgado@wizeline.com>
2022-11-26 00:02:54 -0600

    added cocoa pods

---
+++ RutaSegura/Pods/Target Support Files/Pods-RutaSegura-RutaSeguraUITests/Pods-RutaSegura-RutaSeguraUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RutaSegura_RutaSeguraUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RutaSegura_RutaSeguraUITests
+@end
---
+++ RutaSegura/Pods/Target Support Files/Pods-RutaSeguraTests/Pods-RutaSeguraTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RutaSeguraTests : NSObject
+@end
+@implementation PodsDummy_Pods_RutaSeguraTests
+@end
